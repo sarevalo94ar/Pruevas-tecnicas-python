a9af7a4b18f0df988f798042de42abf7396449a6
sarevalo94119 <sarevalo94119@gmail.com>
2022-06-04 18:48:16 -0500

    tabulado

--- juego.py
+++ juego.py
@@ -4,12 +4,13 @@ print("_____________Juego__________")
 
 
 def piedra_tijera_papel():
+    # es esta opciones el primer jugados siempre pierde
     opciones = {'piedra': 'tijera', 'tijera': 'papel', 'papel': 'piedra'}
     jugador = input("escribe piedra papel o tijera :")
     while jugador not in {'piedra', 'tijera', 'papel'}:
         print("opcion no valida")
         jugador = input("escribe piedra papel o tijera:")
-    
+
     maquina = np.random.choice(['piedra', 'papel', 'tijera'])
     time.sleep(.4)
     print("\n Has selecionado :", jugador)
