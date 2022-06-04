5de969753f9da1645c714b0ad097f1786a27a5ce
sarevalo94119 <sarevalo94119@gmail.com>
2022-06-04 18:12:00 -0500

    mas archivos

---
+++ game.py
@@ -0,0 +1,26 @@
+import numpy as np
+import time
+print("_____________Juego__________")
+
+
+def piedra_tijera_papel():
+    opciones = {'piedra': 'tijera', 'tijera': 'papel', 'papel': 'piedra'}
+    jugador = input("escribe piedra papel o tijera :")
+    while jugador not in {'piedra', 'tijera', 'papel'}:
+        print("opcion no valida")
+        jugador = input("escribe piedra papel o tijera:")
+    
+    maquina = np.random.choice(['piedra', 'papel', 'tijera'])
+    time.sleep(.4)
+    print("\n Has selecionado :", jugador)
+    print("La computadora ha selecionado", maquina)
+    time.sleep(2)
+    if opciones[jugador] == maquina:
+        print("Has ganado")
+    elif opciones[maquina] == jugador:
+        print("!Has perdido")
+    else:
+        print("empate")
+
+
+piedra_tijera_papel()
